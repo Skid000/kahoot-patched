8d1107bf588ea1eccb7d757ff352577328b79d4f
Team OVERSONIC OVERSONIC <49201819+Skid000@users.noreply.github.com>
2022-03-18 15:47:28 +0000

    nothing LOL

--- pen_test.js
+++ pen_test.js
@@ -5,7 +5,7 @@ require('events').EventEmitter.defaultMaxListeners = Infinity;
 var code = prompt("GameCode: ");
 const tag_list = fs.readFileSync('./util/html_tags.txt', 'utf-8').split("\n");
 async function join(code,name,pos){
-    await new Promise(e=>setTimeout(e,pos*30));
+    await new Promise(e=>setTimeout(e,pos*25));
     const client = new Kahoot();
     client.join(code, `<${name}>${name}</${name}>`);
     client.on("Joined", () => {
