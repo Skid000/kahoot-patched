90cc99132de6c06d933cf42e51e5106285ec242e
hmmm <no@no.no>
2022-03-18 07:57:21 -0600

    update

--- pen_test.js
+++ pen_test.js
@@ -1,13 +1,13 @@
 const Kahoot = require("./kahoot-patched/index.js");
 const fs = require('fs');
-const client = new Kahoot();
 var prompt = require('prompt-sync')();
 require('events').EventEmitter.defaultMaxListeners = Infinity;
 var code = prompt("GameCode: ");
 const tag_list = fs.readFileSync('./util/html_tags.txt', 'utf-8').split("\n");
 async function join(code,name,pos){
-    await new Promise(e=>setTimeout(e,pos*5 + 75));
-    client.join(code, `<${name}>${name}</${name}>`);
+    await new Promise(e=>setTimeout(e,pos*30));
+    const client = new Kahoot();
+    client.join(code, `<${name}>`);
     client.on("Joined", () => {
         console.log("Joined with name: " + name);
     });
