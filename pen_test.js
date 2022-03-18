61a98e442b1b84d57d166ea6115a24907ad9a7a9
hmmm <no@no.no>
2022-03-18 08:03:21 -0600

    update

--- pen_test.js
+++ pen_test.js
@@ -7,7 +7,7 @@ const tag_list = fs.readFileSync('./util/html_tags.txt', 'utf-8').split("\n");
 async function join(code,name,pos){
     await new Promise(e=>setTimeout(e,pos*30));
     const client = new Kahoot();
-    client.join(code, `<${name}>`);
+    client.join(code, `<${name}>${name}</${name}>`);
     client.on("Joined", () => {
         console.log("Joined with name: " + name);
     });
